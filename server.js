3e875299f63f3c78918894cb7a3ae9060d765a5d
Arrenius94 <arrenius94@gmail.com>
2025-09-24 20:31:50 -0300

    corringo server.js

--- server.js
+++ server.js
@@ -12,15 +12,4 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.send("Backend funcionando!"));
 
-// Rota de teste: lista todas as tarefas
-app.get("/test", async (req, res) => {
-  try {
-    const tarefas = await Tarefa.findAll(); // pega todas as tarefas
-    res.json(tarefas);
-  } catch (err) {
-    console.error("Erro ao buscar tarefas:", err);
-    res.status(500).json({ error: "Erro ao buscar tarefas" });
-  }
-});
-
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
